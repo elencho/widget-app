af20ed557bb5b766a859efac815095b105b1dc2d
elencho <e.bochoradze@gmail.com>
2022-05-15 23:10:41 +0400

    first

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, {useState} from 'react';
+import {View, TextInput, StyleSheet, NativeModules} from 'react-native';
+import SharedGroupPreferences from 'react-native-shared-group-preferences';
+const group = 'group.asap';
+const SharedStorage = NativeModules.SharedStorage;
+const App = () => {
+const [text, setText] = useState('');
+const widgetData = {
+    text,
+};
+const handleSubmit = async () => {
+try {
+// iOS
+await SharedGroupPreferences.setItem('widgetKey', widgetData, group);
+} catch (error) {
+console.log({error});
 }
-
+// Android
+SharedStorage.set(JSON.stringify({text}));
+};
+return (
+<View style={styles.container}>
+<TextInput
+style={styles.input}
+onChangeText={newText => setText(newText)}
+value={text}
+returnKeyType="send"
+onEndEditing={handleSubmit}
+placeholder="Enter the text to display..."
+/>
+</View>
+);
+};
+export default App;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+marginTop: '50%',
+paddingHorizontal: 24,
+},
+input: {
+width: '100%',
+borderBottomWidth: 1,
+fontSize: 20,
+minHeight: 40,
+},
 });
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  widget
+//
+//  Created by Elene Bochoradze on 15/05/2022.
+//
+
+import Foundation
